cb4d14e3624c6874a070e318549afa56fd891c7d
sergiperez <sergi.perez@iesjoandaustria.org>
2020-04-28 13:14:06 +0000

    Comentaris del professor. Marcats amb TODO

--- JdAssistant.py
+++ JdAssistant.py
@@ -1,21 +1,31 @@
+#TODO Futur mirar de sincronitzar amb GoogleCalendar
+#TODO Millorar estil de programació: noms variables, format, mirar bones pràctiques. 
+#https://docs.google.com/document/d/1PEkRTAWlR4NYJNKJvz9iw4R6m-wJpxDi7xv5tpLPP54/edit 
+
+#TODO Només guarda horari d'un alumne, com ho faràs per guardar més d'un alumne?
+#TODO Pista, diccionaris en Pyhton.
 alumne1=[[],[],[],[],[]]
 dies=["dilluns","dimarts","dimecres","dijous","divendres"]
 hores=["8 a 9" , "9 a 10" , "10 a 11" , "11.30 a 12.30" , "12.30 a 13.30" , "13.30 a 14.30"]
 for d in dies:
     for h in hores:
         print( "Quina materia tens " + d + " de " + h + "?")
-        f=dies.index(d)
+        f=dies.index(d)# Excel·lent
         alumne1[f].append(input().upper())
     print("Els "+d+" l'alumne te "+ str(alumne1[f]) +"; es correcte?")
     resposta=input()
-    resultat=resposta.upper()
+    resultat=resposta.upper()#Cal dues variables?
     if resultat == "NO":
         print("Quin ha sigut l'error?")
+        #TODO Escriure en format més de menú. Posant línia a línia opció.
+        #TODO NO veig utilitat a tantes opcions. I si es mostra bucle de ves 
+        # dient si està bé o no, i cada iteració es canvia una matèria o canviar-lo tot.
         print("Marca la lletra 'a' si l'error és d'una sola matèria; marca la lletra 'b'si l'error és de dues matèries, marca la lletra 'c' si vols tornar a escriure l'horari sencer, marca el numero 1 si vols deixar-ho com esta")
         resposta=input()
         resposta= resposta.upper()
     if resposta == "A":
         print("Indica la matèria incorrecta")
+        #TODO Com vigiles que entri bé el nom de la matèria, un cop posi MATES i altre posi Matemàtiques. 
         canvi=(alumne1[f].index(input().upper()))
         print("Digues la materia que correspon")
         alumne1[f][canvi]=input().upper()
@@ -37,6 +47,8 @@ for d in dies:
         print()
 
 
+#TODO Fer dues funcions diferents, anterior és crear horari
+#TODO QUe ve ara és cercar quan toca una matèria
 #pregunta els dies de cada materia
 num=[0,1,2,3,4]
 pregunta=input("Quins dies tens...").upper()
@@ -46,3 +58,5 @@ for n in num:
         print ("Els "+dies[n]+" tens "+ pregunta.lower()+" de "+ hores[dato])
     else:
         print("Els "+dies[n]+" no hi ha "+pregunta.lower())
+
+#TODO Falta fer funció que digui que toca un dia concret
--- deuresASISTANT.py
+++ deuresASISTANT.py
@@ -1,3 +1,6 @@
+#TODO Futur mirar de sincronitzar amb calendari Moodle
+
+#TODO Per què quatre setmanes?
 stm0=[[[],[],[],[],[],[]],[[],[],[],[],[],[]],[[],[],[],[],[],[]],[[],[],[],[],[],[]],[[],[],[],[],[],[]]]
 stm1=[[[],[],[],[],[],[]],[[],[],[],[],[],[]],[[],[],[],[],[],[]],[[],[],[],[],[],[]],[[],[],[],[],[],[]]]
 stm2=[[[],[],[],[],[],[]],[[],[],[],[],[],[]],[[],[],[],[],[],[]],[[],[],[],[],[],[]],[[],[],[],[],[],[]]]
@@ -7,9 +10,10 @@ dies=["DILLUNS","DIMARTS","DIMECRES","DIJOUS","DIVENDRES"]
 hores=["8-9","9-10","10-11","11:30-12:30","12:30-13:30","13:30-14:30"]
 s=[[]]
 #botó per consultar o per apuntar
-#comentari Sergi
-#comentariAlvaro
+
+#TODO Més que deures és per apuntar dates de lliurament, no?
 for m in s:
+    # TODO Per què bucle? No si no escriu APUNTAR es fa bucle n cops i no s'entèn.
     op=input("Escriu 'apuntar' per poder apuntar deures nous   ").upper()
     if op == "APUNTAR":
         #per apuntar deures, les llistes de dalt representen el calendari d'un mes.
@@ -26,7 +30,7 @@ for m in s:
         s.append([])
         
         
-
+#TODO Posar cada espai en funcions. Diferents.
 
 #consultar deures del mes per dia
 a= input("clica la tecla a per fer una consulta a través del dia ").upper()
@@ -47,5 +51,8 @@ b=input("clica la tecla b per fer una consulta a través de la materia ").upper(
 if b == "B":
     materia=input().upper()
     for d in stm0:
-        if materia in mes or materia in stm0 or materia in stm1 or materia in stm2 or materia in stm3:
+        if materia in mes or materia in stm0 or materia in stm1 or materia in stm2 or materia in stm3:# Excel·lent
             print(str(mes[materia in mes or materia in stm0 or materia in stm1 or materia in stm2 or materia in stm3][1:5][materia.index]))
+
+#TODO Consultar deures per setmana actual
+#TODO Consultar deures per dia actual 
