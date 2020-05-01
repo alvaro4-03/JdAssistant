44d11fa1abbdcea11cb3e2f86818aaf073598b54
alvaro4-03 <alvaro.hernandez@iesjoandaustria.org>
2020-05-01 12:33:42 +0000

    definicions i intent de diccionari, però no funciona del tot bé.

--- JdAssistant.py
+++ JdAssistant.py
@@ -4,38 +4,32 @@
 
 #TODO Només guarda horari d'un alumne, com ho faràs per guardar més d'un alumne?
 #TODO Pista, diccionaris en Pyhton.
-alumne1=[[],[],[],[],[]]
-dies=["dilluns","dimarts","dimecres","dijous","divendres"]
-hores=["8 a 9" , "9 a 10" , "10 a 11" , "11.30 a 12.30" , "12.30 a 13.30" , "13.30 a 14.30"]
-for d in dies:
+alumnes = {}
+alumne1 = [ [] , [] , [] , [] , [] ]
+dies = [ "dilluns" , " dimarts" , "dimecres" , "dijous" , "divendres" ]
+hores = [ "8 a 9" , "9 a 10" , "10 a 11" , "11.30 a 12.30" , "12.30 a 13.30" , "13.30 a 14.30" ]
+numeromateria = [ 0 , 1 , 2 , 3 , 4, 5 ]
+numdia = [ 0 , 1 , 2 , 3 , 4 ]
+def crearHorari():
+  for d in dies:
     for h in hores:
         print( "Quina materia tens " + d + " de " + h + "?")
-        f=dies.index(d)# Excel·lent
+        f = dies.index(d)
         alumne1[f].append(input().upper())
-    print("Els "+d+" l'alumne te "+ str(alumne1[f]) +"; es correcte?")
-    resposta=input()
-    resultat=resposta.upper()#Cal dues variables?
-    if resultat == "NO":
-        print("Quin ha sigut l'error?")
-        #TODO Escriure en format més de menú. Posant línia a línia opció.
-        #TODO NO veig utilitat a tantes opcions. I si es mostra bucle de ves 
-        # dient si està bé o no, i cada iteració es canvia una matèria o canviar-lo tot.
-        print("Marca la lletra 'a' si l'error és d'una sola matèria; marca la lletra 'b'si l'error és de dues matèries, marca la lletra 'c' si vols tornar a escriure l'horari sencer, marca el numero 1 si vols deixar-ho com esta")
-        resposta=input()
-        resposta= resposta.upper()
+    print( "Els "+d+" l'alumne te "+ str(alumne1[f]) +"; es correcte?" )
+    resposta = input( "Escriu si o no " ).upper()
+    if resposta == "NO":
+        print( "Quin ha sigut l'error?" )
+        resposta = input("Marca la lletra 'a' si l'error és d'una sola matèria; marca la lletra 'b'si l'error és de dues matèries, marca la lletra 'c' si vols tornar a escriure l'horari sencer, marca el numero 1 si vols deixar-ho com esta  " )
+        resposta = resposta.upper()
     if resposta == "A":
-        print("Indica la matèria incorrecta")
-        #TODO Com vigiles que entri bé el nom de la matèria, un cop posi MATES i altre posi Matemàtiques. 
-        canvi=(alumne1[f].index(input().upper()))
-        print("Digues la materia que correspon")
-        alumne1[f][canvi]=input().upper()
+        canvi = (alumne1[f].index(input("Indica la matèria incorrecta ").upper()))
+        alumne1[f][canvi] = input("Digues la materia que correspon ").upper()
     if resposta == "B":
-        print("Indica les matèries incorrectes")
-        canvi=(alumne1[f].index(input().upper()))
-        canvi2=(alumne1[f].index(input().upper()))
-        print("Digues les matèries que corresponen")
-        alumne1[f][canvi]=input().upper()
-        alumne1[f][canvi2]=input().upper()
+        canvi = (alumne1[f].index(input("Indica la primera matèria incorrecta ").upper()))
+        canvi2 = (alumne1[f].index(input("Indica la segona matèria incorrecta ").upper()))
+        alumne1[f][canvi] = input("Digues la primera matèria que correspon ").upper()
+        alumne1[f][canvi2] = input("Digues la segona matèria que correspon ").upper()
     if resposta == "C":
         print("Molt bé, ara torna a començar")
         alumne1.clear()
@@ -45,18 +39,33 @@ for d in dies:
         print("Els "+d+" l'alumne te: "+ str(alumne1))
     if resposta == "1":
         print()
-
-
+print(alumnes)
 #TODO Fer dues funcions diferents, anterior és crear horari
 #TODO QUe ve ara és cercar quan toca una matèria
-#pregunta els dies de cada materia
-num=[0,1,2,3,4]
-pregunta=input("Quins dies tens...").upper()
-for n in num:
+def materiaHorari():
+  preguntamateria = input("Quins dies tens...").upper()
+  for n in numdia:
     if pregunta in alumne1[n]:
-        dato= alumne1[n].index(pregunta)
+        dato = alumne1[n].index(preguntamateria)
         print ("Els "+dies[n]+" tens "+ pregunta.lower()+" de "+ hores[dato])
     else:
         print("Els "+dies[n]+" no hi ha "+pregunta.lower())
-
 #TODO Falta fer funció que digui que toca un dia concret
+def diaHorari():
+  preguntadia = input("Quin dia vols saber? ").lower()
+  for n in numeromateria:
+    if preguntadia in dies:
+      dia = dies.index(preguntadia)
+      print("Els " + preguntadia + " tens " + alumne1[dia][n])
+
+print("Les funcions disponibles són: /crearhorari o /consultarmateria i /consultardia si ja has creat l'horari")
+print("Escriu la funció que vulguis utilitzar")
+funcio = input().upper()
+if funcio == "/CREARHORARI":
+  alumnes[input("Posa el teu nom ")] = str(crearHorari())
+elif funcio == "/CONSULTARMATERIA":
+  materiaHorari()
+elif funcio == "/CONSULTARDIA":
+  diaHorari()
+
+print(alumnes)
---
+++ funcions.py
@@ -0,0 +1,25 @@
+def sumar(numero1, numero2):
+  resultat = numero1 + numero2
+  return resultat
+
+def aixecar():
+  print("Àlvaro s'aixeca")
+
+
+#num1 = int(input("A"))
+#num2 = int(input("B"))
+#print(str(sumar(num1,num2))+str(sumar(78,89)))
+#aixecar()
+
+#porra = {clau:valor}
+
+#cognoms = {"Harun":"meraga"}
+#edat = {"Harun":17}
+#porra = {"Harun":{"Edat":13,"Cognom":"Meraga","Porra":[0,0]}}
+
+#print(porra["Harun"]["Edat"])
+#print(porra)
+
+prova = {}
+prova["hola"]= 10
+print(prova)
