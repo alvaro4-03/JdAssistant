b1f81c70e3151e3fb1433e14a64a8b54eb92a98a
Alvaro Hernandez <alvaro.hernandez@iesjoandaustria.org>
2020-10-23 14:37:04 +0000

    prova 23-10

--- JdAssistantBot.py
+++ JdAssistantBot.py
@@ -45,15 +45,7 @@ def triarDia(missatge):
   bot.register_next_step_handler(resposta1, EscriureDeures)
   
 def EscriureDeures(missatge):
-  def echo_all(missatge):
-	#Tractar només si t'interessa, ha fet camñí que tocar.
-  #if comanda[missatge.missatge.from_user.username] == "apuntar deures":
-      #Posar missatge.text al diccionari de deures
-      bot.send_message(missatge.chat.id, input())
-  #llistat_alumnes = {alummne : {materia : [[dia,tasca][]]}}
-  markup = types.ReplyKeyboardMarkup(row_width=1, one_time_keyboard=True)
-  markup.add(types.KeyboardButton("Seguir"))
-  resposta1=bot.send_message(missatge.chat.id,"Fes clic al botó per seguir",reply_markup=markup)
+  
   #Escriure els deures
   bot.register_next_step_handler(resposta1, Revisar)
 
