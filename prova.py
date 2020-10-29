b4cac37234f85d1b7d135c266ff5ef02a162323e
Alvaro Hernandez <alvaro.hernandez@iesjoandaustria.org>
2020-10-29 17:51:28 +0000

    prova 7 29/10

--- prova.py
+++ prova.py
@@ -6,59 +6,61 @@ from telebot import types
 
 #TODO
 #Token que lliga amb el bot de Telegram
-TOKEN = '1376336790:AAFcTgWbf-4pJNejrAGknJSxZzuaCI6wXj0' 
+TOKEN = '970292498:AAFB32dEsoBGBCnqJyIeXL7urpCHGjCQOZg' 
 bot = telebot.TeleBot(TOKEN)
 
 #Diccionari per guardar deures
 
-#Diccionari per guardar comanda i username
-comandes = {}
 #Comencen comandes
 @bot.message_handler(commands=['agenda'])
 def agenda_command(missatge):
+ # markup = types.InlineKeyboardMarkup()
+  #ui = types.InlineKeyboardButton('Private',url='https://telegram.me/mosydev')
+  #op = types.InlineKeyboardButton('Bot',url='https://telegram.me/mosydevbot')
+  #markup.add(ui)
+  #markup.add(op)
+  #bot.send_message(missatge.chat.id,'Our Bot And Owner Private',reply_markup=markup)
+	#missatge m'arriba tota la info del xat que estic fent servir
   missatge.from_user.username
-  markup = types.ReplyKeyboardMarkup(row_width=3, one_time_keyboard=True)
-  markup.add(types.KeyboardButton("Mates") , types.KeyboardButton("Català") , types.KeyboardButton("Socials" , types.KeyboardButton("Castellà") , types.KeyboardButton("Naturals/Bio") , types.KeyboardButton("Fisica") , types.KeyboardButton("Filo/Etica") , types.KeyboardButton("Quimica") , types.KeyboardButton("Agenda") , types.KeyboardButton("EF") , types.KeyboardButton("CMC") , types.KeyboardButton("Eco") , types.KeyboardButton("Eco Empresa") , types.KeyboardButton("Tecno") , types.KeyboardButton("English") , types.KeyboardButton("Musica") , types.KeyboardButton("Plastica") , types.KeyboardButton("Optativa"))
-  resposta=bot.send_message(missatge.chat.id,"Escull la matèria dels deures que vols apuntar",reply_markup=markup)
+  #identificador conversa missatge.chat.id
+  #TODO fer que apareixin les matèries amb una botonera
+
+  #Fem botonera
+  markup = types.ReplyKeyboardMarkup(row_width=2, one_time_keyboard=True)
+  markup.add(types.KeyboardButton("Mates"),types.KeyboardButton("Català"),types.KeyboardButton("Socials"),types.KeyboardButton("Castellà"),types.KeyboardButton("Naturals/Bio"),types.KeyboardButton("Fisica"),types.KeyboardButton("Filo/Etica"),types.KeyboardButton("Quimica"),types.KeyboardButton("Agenda"),types.KeyboardButton("EF"),types.KeyboardButton("CMC"),types.KeyboardButton("Eco"),types.KeyboardButton("Eco Empresa"),types.KeyboardButton("Tecno"),types.KeyboardButton("English"),types.KeyboardButton("Musica"),types.KeyboardButton("Plastica"),types.KeyboardButton("Optativa"))
+
+
+  #Creem la resposta
+  #identificador conversa, missatge , la botonera 
+  resposta=bot.send_message(missatge.chat.id,"Escull la matèria que vols apuntar",reply_markup=markup)
+
+  #Li dic que quan toqui un botó que ha de fer
+  #envies la resposta i quan toqui botó fan la funció jugar
   bot.register_next_step_handler(resposta, triarDia)
 
-#def triarDia(missatge):
+def triarDia(missatge):
   # hauràs d¡inserir usuari i resposta triada de matària
   #El botó (matèria) triat per l'usuari està a missatge.text
   alumne = missatge.from_user.username
-  materia = missatge.text
   #Omplir diccionari
+  #{alumne:{missatge.text: [[dia,tasca],[]]}}
   #Demanar dia
-  #markup = types.ReplyKeyboardMarkup(row_width=1, one_time_keyboard=True)
-  #markup.add(types.KeyboardButton("Dilluns"),types.KeyboardButton("Dimarts"),types.KeyboardButton("Dimecres"),types.KeyboardButton("Dijous"),types.KeyboardButton("Divendres"))
-  #resposta1=bot.send_message( missatge.chat.id , "Escull el dia per al que apuntar" , reply_markup=markup )
+  markup = types.ReplyKeyboardMarkup(row_width=5, one_time_keyboard=True)
+  markup.add(types.KeyboardButton("Dll"),types.KeyboardButton("Dm"),types.KeyboardButton("Dc"),types.KeyboardButton("Dj"),types.KeyboardButton("Dv"))
+  resposta1=bot.send_message(missatge.chat.id,"Escull el dia per al que apuntar",reply_markup=markup)
   #Escriure els deures
-  #bot.register_next_step_handler(resposta1, EscriureDeures)
-  
-#def EscriureDeures(missatge):
-  #@bot.message_handler(func=lambda message: True)
-  #def echo_all(missatge):
-    #resposta = bot.send_message(missatge.chat.id )
-    #bot.send_message(missatge.chat.id, "Has apuntat " + resposta)
-	#Tractar només si t'interessa, ha fet camñí que tocar.
-  #if comanda[missatge.missatge.from_user.username] == "apuntar deures":
-   #Posar missatge.text al diccionari de deures
-  #Escriure els deures
-  #bot.register_next_step_handler(resposta1)
+  bot.register_next_step_handler(resposta1, EscriureDeures)
+  return ""
+
+def EscriureDeures(missatge):
+
+  return ""
 
 #Comencen comandes
-#@bot.message_handler(commands=['mostraDeures'])
-#def mostraDeures_command(missatge):  
+@bot.message_handler(commands=['mostraDeures'])
+def mostraDeures_command(missatge):  
   #Pintaràs els deures que tens guardats
   #Triant el diccionari que correspon a missatge.from_user.username
- # return ""
-
-
-#@bot.message_handler(func=lambda message: True)
-#def echo_all(missatge):
-	#Tractar només si t'interessa, ha fet camñí que tocar.
-  #if comanda[missatge.missatge.from_user.username] == "apuntar deures":
-      #Posar missatge.text al diccionari de deures
-      
+  return ""
 
 bot.polling()
