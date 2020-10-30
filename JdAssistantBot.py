c2f464b4e0d5f36e97a11dd94e951f827f2da879
Ubuntu <ubuntu@ip-172-31-84-15.ec2.internal>
2020-10-30 16:32:26 +0000

    Millora del problema resposta

--- JdAssistantBot.py
+++ JdAssistantBot.py
@@ -18,40 +18,40 @@ comandes = {}
 def agenda_command(missatge):
   missatge.from_user.username
   markup = types.ReplyKeyboardMarkup(row_width=3, one_time_keyboard=True)
-  markup.add(types.KeyboardButton("Mates") , types.KeyboardButton("Català") , types.KeyboardButton("Socials" , types.KeyboardButton("Castellà") , types.KeyboardButton("Naturals/Bio") , types.KeyboardButton("Fisica") , types.KeyboardButton("Filo/Etica") , types.KeyboardButton("Quimica") , types.KeyboardButton("Agenda") , types.KeyboardButton("EF") , types.KeyboardButton("CMC") , types.KeyboardButton("Eco") , types.KeyboardButton("Eco Empresa") , types.KeyboardButton("Tecno") , types.KeyboardButton("English") , types.KeyboardButton("Musica") , types.KeyboardButton("Plastica") , types.KeyboardButton("Optativa"))
+  markup.add(types.KeyboardButton("Mates") , types.KeyboardButton("Català") , types.KeyboardButton("Socials") , types.KeyboardButton("Castellà") , types.KeyboardButton("Naturals/Bio") , types.KeyboardButton("Fisica") , types.KeyboardButton("Filo/Etica") , types.KeyboardButton("Quimica") , types.KeyboardButton("Agenda") , types.KeyboardButton("EF") , types.KeyboardButton("CMC") , types.KeyboardButton("Eco") , types.KeyboardButton("Eco Empresa") , types.KeyboardButton("Tecno") , types.KeyboardButton("English") , types.KeyboardButton("Musica") , types.KeyboardButton("Plastica") , types.KeyboardButton("Optativa"))
   resposta=bot.send_message(missatge.chat.id,"Escull la matèria dels deures que vols apuntar",reply_markup=markup)
   bot.register_next_step_handler(resposta, triarDia)
 
-#def triarDia(missatge):
+def triarDia(missatge):
   # hauràs d¡inserir usuari i resposta triada de matària
   #El botó (matèria) triat per l'usuari està a missatge.text
   alumne = missatge.from_user.username
   materia = missatge.text
   #Omplir diccionari
   #Demanar dia
-  #markup = types.ReplyKeyboardMarkup(row_width=1, one_time_keyboard=True)
-  #markup.add(types.KeyboardButton("Dilluns"),types.KeyboardButton("Dimarts"),types.KeyboardButton("Dimecres"),types.KeyboardButton("Dijous"),types.KeyboardButton("Divendres"))
-  #resposta1=bot.send_message( missatge.chat.id , "Escull el dia per al que apuntar" , reply_markup=markup )
+  markup = types.ReplyKeyboardMarkup(row_width=1, one_time_keyboard=True)
+  markup.add(types.KeyboardButton("Dilluns"),types.KeyboardButton("Dimarts"),types.KeyboardButton("Dimecres"),types.KeyboardButton("Dijous"),types.KeyboardButton("Divendres"))
+  resposta1=bot.send_message( missatge.chat.id , "Escull el dia per al que apuntar" , reply_markup=markup )
   #Escriure els deures
-  #bot.register_next_step_handler(resposta1, EscriureDeures)
+  bot.register_next_step_handler(resposta1, EscriureDeures)
   
-#def EscriureDeures(missatge):
-  #@bot.message_handler(func=lambda message: True)
-  #def echo_all(missatge):
-    #resposta = bot.send_message(missatge.chat.id )
-    #bot.send_message(missatge.chat.id, "Has apuntat " + resposta)
+def EscriureDeures(missatge):
+  @bot.message_handler(func=lambda message: True)
+  def echo_all(missatge):
+    resposta = bot.send_message(missatge.chat.id )
+    bot.send_message(missatge.chat.id, "Has apuntat " + resposta)
 	#Tractar només si t'interessa, ha fet camñí que tocar.
   #if comanda[missatge.missatge.from_user.username] == "apuntar deures":
    #Posar missatge.text al diccionari de deures
   #Escriure els deures
-  #bot.register_next_step_handler(resposta1)
+  bot.register_next_step_handler(resposta1)
 
 #Comencen comandes
-#@bot.message_handler(commands=['mostraDeures'])
-#def mostraDeures_command(missatge):  
+@bot.message_handler(commands=['mostraDeures'])
+def mostraDeures_command(missatge):  
   #Pintaràs els deures que tens guardats
   #Triant el diccionari que correspon a missatge.from_user.username
- # return ""
+  return ""
 
 
 #@bot.message_handler(func=lambda message: True)
@@ -59,6 +59,6 @@ def agenda_command(missatge):
 	#Tractar només si t'interessa, ha fet camñí que tocar.
   #if comanda[missatge.missatge.from_user.username] == "apuntar deures":
       #Posar missatge.text al diccionari de deures
-      
+ 
 
 bot.polling()
