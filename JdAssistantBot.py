cf7c370e7d7b39e170053331d19cabf6f03ed6cc
Ubuntu <alvaro.hernandez@iesjoandaustria.org>
2020-11-02 18:08:11 +0000

    Resposta bot amb deures apuntats

--- JdAssistantBot.py
+++ JdAssistantBot.py
@@ -18,16 +18,9 @@ bot = telebot.TeleBot(TOKEN)
 def agenda_command(missatge):
 
   markup = types.ReplyKeyboardMarkup(row_width=3, one_time_keyboard=True)
-  markup.add(types.KeyboardButton("Mates") , types.KeyboardButton("Català") , types.KeyboardButton("Socials") , types.KeyboardButton("Castellà") , types.KeyboardButton("Naturals/Bio") , types.KeyboardButton("Fisica") , types.KeyboardButton("Filo/Etica") , types.KeyboardButton("Quimica") , types.KeyboardButton("Agenda") , types.KeyboardButton("EF") , types.KeyboardButton("CMC") , types.KeyboardButton("Eco") , types.KeyboardButton("Eco Empresa") , types.KeyboardButton("Tecno") , types.KeyboardButton("English") , types.KeyboardButton("Musica") , types.KeyboardButton("Plastica") , types.KeyboardButton("Optativa"))
-  resposta=bot.send_message(missatge.chat.id,"Escull la matèria dels deures que vols apuntar",reply_markup=markup)
-  bot.register_next_step_handler(resposta, triarDia)
-
-def triarDia(missatge):
-  markup.add(types.KeyboardButton("Mates"),types.KeyboardButton("Català"),types.KeyboardButton("Socials"),types.KeyboardButton("Castellà"),types.KeyboardButton("Naturals/Bio"),types.KeyboardButton("Fisica"),types.KeyboardButton("Filo/Etica"),types.KeyboardButton("Quimica"),types.KeyboardButton("Agenda"),types.KeyboardButton("EF"),types.KeyboardButton("CMC"),types.KeyboardButton("Eco"),types.KeyboardButton("Eco Empresa"),types.KeyboardButton("Tecno"),types.KeyboardButton("English"),types.KeyboardButton("Musica"),types.KeyboardButton("Plastica"),types.KeyboardButton("Optativa"))
-  bot.register_next_step_handler(resposta, triarDia)
-
+  markup.add(types.KeyboardButton("Mates"),types.KeyboardButton("Català"),types.KeyboardButton("Socials"),types.KeyboardButton("Castellà"),types.KeyboardButton("Naturals/Bio"),types.KeyboardButton("Fisica"),types.KeyboardButton("Filo/Etica"),types.KeyboardButton("Quimica"), types.KeyboardButton("EF"),types.KeyboardButton("CMC"),types.KeyboardButton("Eco"),types.KeyboardButton("Eco Empresa"),types.KeyboardButton("Tecno"),types.KeyboardButton("English"),types.KeyboardButton("Musica"),types.KeyboardButton("Plastica"),types.KeyboardButton("Optativa"))
   resposta=bot.send_message(missatge.chat.id,"Escull la matèria que vols apuntar",reply_markup=markup)
-
+  bot.register_next_step_handler(resposta, triarDia)
 def triarDia(missatge):
   # hauràs d¡inserir usuari i resposta triada de matària
   #El botó (matèria) triat per l'usuari està a missatge.text
@@ -38,23 +31,29 @@ def triarDia(missatge):
   markup = types.ReplyKeyboardMarkup(row_width=1, one_time_keyboard=True)
   markup.add(types.KeyboardButton("Dilluns"),types.KeyboardButton("Dimarts"),types.KeyboardButton("Dimecres"),types.KeyboardButton("Dijous"),types.KeyboardButton("Divendres"))
   resposta1=bot.send_message( missatge.chat.id , "Escull el dia per al que apuntar" , reply_markup=markup )
+
   #Escriure els deures
   bot.register_next_step_handler(resposta1, EscriureDeures)
-  
-def EscriureDeures(missatge):
+
+def EscriureDeures (missatge):
+  resposta2 = bot.send_message(missatge.chat.id, "Escriu els  deures que vols que s'apuntin")
+
   @bot.message_handler(func=lambda message: True)
   def echo_all(missatge):
-    resposta = bot.send_message(missatge.chat.id )
-    bot.send_message(missatge.chat.id, "Has apuntat " + resposta)
+    resposta3 = missatge.text
+    deure = bot.send_message(missatge.chat.id, "Has apuntat " + resposta3)
 	#Tractar només si t'interessa, ha fet camñí que tocar.
   #if comanda[missatge.missatge.from_user.username] == "apuntar deures":
    #Posar missatge.text al diccionari de deures
   #Escriure els deures
-  bot.register_next_step_handler(resposta1)
+  #bot.register_next_step_handler(resposta1)
+
 
 #Comencen comandes
-@bot.message_handler(commands=['mostraDeures'])
-def mostraDeures_command(missatge):  
+@bot.message_handler(commands=['consultaragenda'])
+def consultaragenda_command(missatge): 
+  deures_apuntats = missatge.text
+  consulta = bot.send_message(missatge.chat.id, deures_apuntats)
   #Pintaràs els deures que tens guardats
   #Triant el diccionari que correspon a missatge.from_user.username
   return ""
@@ -65,6 +64,5 @@ def mostraDeures_command(missatge):
 	#Tractar només si t'interessa, ha fet camñí que tocar.
   #if comanda[missatge.missatge.from_user.username] == "apuntar deures":
       #Posar missatge.text al diccionari de deures
- 
 
 bot.polling()
